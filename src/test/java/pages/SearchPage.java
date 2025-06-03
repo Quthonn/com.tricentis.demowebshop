5b4d68e7b62a1527316f64083eee2f741d0c9ad2
quthonn <gevorgyan.genri@gmail.com>
2025-06-03 17:17:04 +0700

    Added search test

---
+++ src/test/java/pages/SearchPage.java
@@ -0,0 +1,27 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Allure;
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SearchPage {
+    private SelenideElement
+            searchField = $(".search-box-text"),
+            placeWithProducts = $(".search-results");
+
+    public SearchPage searchProduct(String query) {
+        Allure.step("Ввод названия товара в поле поиска и нажатие Enter", () -> {
+            searchField.setValue(query).pressEnter();
+        });
+        return this;
+    }
+
+    public SearchPage viewFoundedProducts(String query) {
+        Allure.step("Проверка найденных товаров", () -> {
+            String actualText = placeWithProducts.getText().toLowerCase();
+            assertTrue(actualText.contains(query));
+        });
+        return this;
+    }
+}
---
+++ src/test/java/uiTests/SearchProduct.java
@@ -0,0 +1,27 @@
+package uiTests;
+
+import config.TestBase;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import pages.SearchPage;
+
+public class SearchProduct extends TestBase {
+    SearchPage searchPage = new SearchPage();
+
+    @Feature("Тестирование поиска")
+    @Story("Пользователь в поиске вводит название товара")
+    @Severity(SeverityLevel.NORMAL)
+    @Tag("positive")
+    @DisplayName("Тест поиска")
+    @Owner("Quthon")
+    @Description("Этот тест проверяет, что пользователь может в поиске все найти.")
+    @ParameterizedTest(name = "Поиск по слову")
+    @ValueSource(strings = {"laptop", "book"})
+    public void search(String query) {
+        searchPage.searchProduct(query)
+                .viewFoundedProducts(query);
+    }
+}
